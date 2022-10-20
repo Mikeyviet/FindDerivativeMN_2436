da8ec0a9ff1d4c57059b7744791d482f8aaf2e81
mikeyviet <vietwiin@gmail.com>
2022-10-19 23:02:25 -0500

    first commit

---
+++ src/main/java/quiz2ec/findderivativemn/FindDerivativeMN.java
@@ -0,0 +1,30 @@
+/**
+ * @Name    Michael Nguyen
+ * @Course  COSC 2436
+ * @Assign. Quiz 2 Extra Credit (Problem 1)
+ * @Description Write a program to help find the derivative of polynomial expressions with simple powers (no negative or fractional exponents). 
+ *      The rules of finding a derivative are the following:
+ * 
+ *          • If the expression is a constant value, the derivative is zero.
+ *          • If the expression is any power in the form AX^N, where is A is a positive or negative integer, and N is any positive integer, the 
+ *              derivative is equal to A times N, times X raised to the power of N-1, or ANX^(N-1). 
+ *          • The derivative of a sum of multiple operands is the sum of the derivatives of each operand.
+ *      
+ *      The program must implement the linked list data structure to store the polynomial expression. 
+ *      program must also implement a stack   
+ */
+
+//package quiz2ec.findderivativemn;
+
+/**
+ *
+ * @author michaelnguyen
+ */
+public class FindDerivativeMN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+    }
+}
